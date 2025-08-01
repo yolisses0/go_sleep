cd9f2fed532be6d24d6f3c82014756866db920bf
Yolisses <yolisses0@gmail.com>
2025-08-01 17:05:13 -0300

    Create a button

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,15 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: TextButton(
+            onPressed: () {
+              print('test1');
+            },
+            child: Text('Press me'),
+          ),
         ),
       ),
     );
